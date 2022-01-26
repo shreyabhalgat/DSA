62ddb63a9d79c284ebcecf7f4c9498f57ed02089
Shreya Bhalgat <bhalgatshreya2@gmail.com>
2022-01-26 19:44:09 +0530

    Added factorial in recurssion dir

---
+++ Recurssion/factorial.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+
+    int smalloutput=fact(n-1);
+    return n*smalloutput;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int output=fact(n);
+    cout<<"factorial of the given number is"<<output;
+    return 0;
+}
